b8b996aa2b7b0f9dc9a920121227413c1e9ddfaf
hedevedup <uzomaesse@gmail.com>
2022-10-29 13:43:44 +0100

    feat: added validation to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :name, presence: true
 
-
-
-  has_many :budgets, foreign_key: :user_id, dependent: :destroy
-  has_many :groups, dependent: :destroy
-
-
-  validates :name, presence: true
 end
